fba44a4cef098fe1efcfd635f5c8c8142ba289e6
George <geomois@gmail.com>
2017-01-16 22:24:39 +0100

    to json

--- extractTitle.py
+++ extractTitle.py
@@ -2,6 +2,7 @@ import os
 import sys
 import numpy as np
 import imdb
+import json
 
 def fileWalker(directory,file):
     filesToScrape=[]
@@ -10,7 +11,7 @@ def fileWalker(directory,file):
             filesToScrape.append(d)
 
     uniqueTitle=np.unique(filesToScrape)
-    export(file,uniqueTitle)
+    exportTxt(file,uniqueTitle)
 
 def mergeFiles(file1,file2):
     titles=[]
@@ -23,7 +24,7 @@ def mergeFiles(file1,file2):
             titles.append(line)
     
     uniqueTitle=np.unique(titles)
-    export(file1,uniqueTitle)
+    exportTxt(file1,uniqueTitle)
     
 def readLst(directory,exportFile):
     # TODO:implement function for extracting codes from hts_cache/*.lst files
@@ -39,10 +40,50 @@ def readLst(directory,exportFile):
                             titles.append(fooArray[2][:9])
     
     uniqueTitle=np.unique(titles)
-    export(exportFile,uniqueTitle)
+    exportTxt(exportFile,uniqueTitle)
 
+def fetchMovieInfo(titleList,exportFile):
+    im=imdb.IMDb()
+    movieList=[]
+    for i in xrange(0,len(titleList)):
+        result=im.get_movie(titleList[i])
+        # print titleList[i],"Fetched "+str(result)
+        if str(result)!='':
+           movieList.append(result.data)
+        else:
+            # print "ID does not exist",titleList[i]
+            if i>=titleList[len(titleList)-1]:
+                break
 
-def export(exportFile,titleList):
+        if (i+1)%1000==0:
+            print "Writing at "+str(i)
+            movieList=cleanData(movieList)
+            exportJson(str(i+1)+".json",movieList)
+            movieList=[]
+
+    movieList=cleanData(movieList)
+    exportJson(exportFile,movieList)
+    
+def cleanData(movieList):
+    fooList=[]
+    for i in xrange(0,len(movieList)):
+        fooDict=movieList[i]
+        for key in fooDict:
+            item=fooDict[key]
+            if type(item)==list:
+                tempList=[]
+                for i in xrange(0,len(item)):
+                    if type(item[i])!=unicode:
+                        tempString=str(item[i])
+                        # tempString=u''.join(item[i]).encode('UTF-8')
+                    else:
+                        tempString=item[i]
+                    tempList.append(tempString)
+                fooDict[key]=tempList
+        fooList.append(fooDict)
+    return fooList
+
+def exportTxt(exportFile,titleList):
     count=0
     if not os.path.isfile(exportFile):
         f=open(exportFile,"w+")
@@ -56,14 +97,24 @@ def export(exportFile,titleList):
 
     print count,"titles written in", exportFile
 
-def fetchMovieInfo(titleList):
-    # im=imdb.IMDb()
-    # result=im.get_movie(titleCode)
-    # ####im.update(result)
-    # result.data
-    pass
+def exportJson(exportFile,movies):
+    count=0
+    if not os.path.isfile(exportFile):
+        f=open(exportFile,"w+")
+        f.close()
+
+    with open(exportFile,'a+') as f:
+        for movie in movies:
+            count+=1
+            # json.dump(movie,f,sort_keys=True, indent=2)
+            json.dump(movie,f)
+            f.write('\n')
+
+    print count,"titles written in", exportFile
 
 if __name__ == '__main__':
+    reload(sys)
+    sys.setdefaultencoding('UTF8')
     if(sys.argv[1]=="-m"):
         mergeFiles(sys.argv[2],sys.argv[3])
     elif (sys.argv[1]=="-d"):
@@ -71,4 +122,7 @@ if __name__ == '__main__':
         print files
     elif (sys.argv[1]=="-l"):
         readLst(sys.argv[2],sys.argv[3])
+    elif (sys.argv[1]=="-f"):
+        codeList=np.arange(int(sys.argv[3]),6500000)
+        fetchMovieInfo(codeList,sys.argv[2])
         
