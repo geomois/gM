e60043e090bdbb778704dc448372ac0dece5e84e
JohnTheo13 <theodosioujohn88@gmail.com>
2017-02-16 16:57:53 +0100

    ready for testing

--- movieApp/js/controllers.js
+++ movieApp/js/controllers.js
@@ -1,5 +1,5 @@
 app.controller('TimerCtrl', ['$scope', '$timeout', 'CookieService',
-    function($scope, $timeout, CookieService) {
+    function ($scope, $timeout, CookieService) {
         $scope.bar = 100;
         $scope.counter = 120;
         $scope.count = '';
@@ -13,7 +13,7 @@ app.controller('TimerCtrl', ['$scope', '$timeout', 'CookieService',
             var teamC = 'team2';
         }
 
-        $scope.onTimeout = function() {
+        $scope.onTimeout = function () {
             if ($scope.counter < 80) {
                 $timeout.cancel(mytimeout);
                 $scope.count = 'none';
@@ -27,18 +27,18 @@ app.controller('TimerCtrl', ['$scope', '$timeout', 'CookieService',
 
         var mytimeout = $timeout($scope.onTimeout, 1000);
 
-        $scope.stop = function() {
+        $scope.stop = function () {
             $timeout.cancel(mytimeout);
         }
 
-        $scope.start = function() {
+        $scope.start = function () {
             mytimeout = $timeout($scope.onTimeout, 400);
         }
-        $scope.paused = function(team) {
+        $scope.paused = function (team) {
             CookieService.updateCookies(teamC);
             CookieService.updateCookiesTeams();
         }
-        $scope.notPassed = function() {
+        $scope.notPassed = function () {
             CookieService.updateCookiesTeams();
         }
 
@@ -46,12 +46,40 @@ app.controller('TimerCtrl', ['$scope', '$timeout', 'CookieService',
 ]);
 
 
-app.controller('StartCtrl', ['$scope', 'CookieService',
-    function($scope, CookieService) {
+app.controller('StartCtrl', ['$scope', '$rootScope', 'CookieService', 'GetMovieData',
+    function ($scope, $rootScope, CookieService, GetMovieData) {
+
+        GetMovieData.movieData(function (dataResponse) {
+            //$scope.movies = dataResponse;
+            $rootScope.movies1 = dataResponse.slice(0, 20)
+            $rootScope.movies2 = dataResponse.slice(20, dataResponse.length)
+            $rootScope.genres = [
+                "Thriller", "Horror",
+                "Biography", "History",
+                "Sci-Fi", "Fantasy",
+                "Crime", "Drama", /*POSTER*/
+                "Musical", "Music",
+                "Romance", "Film-Noir",/*POSTER*/
+                "Mystery", "Action", "Adventure", "Western", "War",
+                "Sport",/*POSTER*/
+                "Family", "Animation", "Comedy"];
+
+        });
+        $rootScope.poster = {
+            "horror": "scary.png",
+            "action": "adventure.png",
+            "family": "families.png",
+            "fantasy": "fantasy.png",
+            "history": "history.png",
+            "music": "musical.png",
+            "crimeDrama":"crime.png",
+            "noirRomance":"romance.png",
+            "sport":"sport.png"
+        }
 
         $scope.team1 = '';
         $scope.team2 = '';
-        $scope.save = function() {
+        $scope.save = function () {
             var team_2 = {
                 name: $scope.team2,
                 score: 0,
@@ -71,60 +99,78 @@ app.controller('StartCtrl', ['$scope', 'CookieService',
     }
 ]);
 
-app.controller('GameCtrl', ['$timeout', '$scope', '$rootScope', 'GetMovieData', 'CookieService', 'GameService',
-    function($timeout, $scope, $rootScope, GetMovieData, CookieService, GameService) {
+app.controller('GameCtrl', ['$timeout', '$scope', '$rootScope', 'CookieService', 'GameService',
+    function ($timeout, $scope, $rootScope, CookieService, GameService) {
 
         var teams = CookieService.getCookies('teamCount');
-        if (!$rootScope.movies1 && !$rootScope.movies2) {
-            GetMovieData.movieData(function(dataResponse) {
-                //$scope.movies = dataResponse;
-                $rootScope.movies1 = dataResponse.slice(0, 20)
-                $rootScope.movies2 = dataResponse.slice(20, dataResponse.length)
-
-
-                if (isEven(teams)) {
-                    $scope.team = CookieService.getCookies('team1');
-                    $scope.teamC = 'team1';
-                    $scope.movie = GameService.game($scope.team.movies, $rootScope.movies1);
-                } else {
-                    $scope.team = CookieService.getCookies('team2');
-                    $scope.teamC = 'team2';
-                    $scope.movie = GameService.game($scope.team.movies, $rootScope.movies2);
-                }
-
-                console.log($rootScope.movies1, $rootScope.movies2)
-                
-            });
-        }else{
-            if (isEven(teams)) {
-                    $scope.team = CookieService.getCookies('team1');
-                    $scope.teamC = 'team1';
-                    $scope.movie = GameService.game($scope.team.movies, $rootScope.movies1);
-                } else {
-                    $scope.team = CookieService.getCookies('team2');
-                    $scope.teamC = 'team2';
-                    $scope.movie = GameService.game($scope.team.movies, $rootScope.movies2);
-                }
+        if (isEven(teams)) {
+            $scope.team = CookieService.getCookies('team1');
+            $scope.teamC = 'team1';
+            $scope.movie = GameService.game($scope.team.movies, $rootScope.movies1);
+        } else {
+            $scope.team = CookieService.getCookies('team2');
+            $scope.teamC = 'team2';
+            $scope.movie = GameService.game($scope.team.movies, $rootScope.movies2);
         }
-     $scope.updateTeam = function(id) {
-                    CookieService.teamArray($scope.teamC, id);
-            }
+        
+        var genre = $scope.movie.Genre;
+        console.log(genre)
+        switchGendre($rootScope.genres, genre, function (data) {
+            $scope.src = switchRes(data, $rootScope.genres, $rootScope.poster);
+        });
+
+
+
+        $scope.updateTeam = function (id) {
+            CookieService.teamArray($scope.teamC, id);
+        }
 
     }
 ]);
 
 app.controller('OverviewCrtl', ['$scope', '$cookies', 'CookieService', '$rootScope', 'GetMovieData', '$timeout',
-    function($scope, $cookies, CookieService, $rootScope, GetMovieData, $timeout) {
+    function ($scope, $cookies, CookieService, $rootScope, GetMovieData, $timeout) {
         $scope.team1 = CookieService.getCookies('team1');
         $scope.team2 = CookieService.getCookies('team2');
-
+        
+        $scope.displayRound = $scope.team2.movies.length+1 > 20 ? 'none' : null
+        $scope.display = $scope.team2.movies.length+1 > 20 ? null : 'none'
+        if ($scope.display===null){
+            console.log($scope.team1.score,$scope.team2.score)
+            if ($scope.team1.score == $scope.team2.score){
+                $scope.winner = 'DrAw'
+            }else{
+                $scope.winner = $scope.team1.score > $scope.team2.score ? $scope.team1.name : $scope.team2.score
+            }
+        }
         if (!$rootScope.movies1 && !$rootScope.movies2) {
-            GetMovieData.movieData(function(dataResponse) {
+            GetMovieData.movieData(function (dataResponse) {
                 //$scope.movies = dataResponse;
                 $rootScope.movies1 = dataResponse.slice(0, 20)
                 $rootScope.movies2 = dataResponse.slice(20, dataResponse.length)
-            });
+                $rootScope.genres = [
+                    "Thriller", "Horror",
+                    "Biography", "History",
+                    "Sci-Fi", "Fantasy",
+                    "Crime", "Drama", /*POSTER*/
+                    "Musical", "Music",
+                    "Romance", "Film-Noir",/*POSTER*/
+                    "Mystery", "Action", "Adventure", "Western", "War",
+                    "Sport",/*POSTER*/
+                    "Family", "Animation", "Comedy"];
 
+                $rootScope.poster = {
+                    "horror": "scary.png",
+                    "action": "adventure.png",
+                    "family": "families.png",
+                    "fantasy": "fantasy.png",
+                    "history": "history.png",
+                    "music": "musical.png",
+                    "crimeDrama":"crime.png",
+                    "noirRomance":"romance.png",
+                    "sport":"sport.png"
+                }
+            });
         }
     }
 ]);
@@ -137,4 +183,74 @@ var test = 'fdfdfdf'
 
 function arraySlice(arr, x) {
     return arr.slice(x, (x + 5));
+}
+
+function switchGendre(array, str, callbackFunc) {
+    for (i in array) {
+        if (str.search(array[i]) > -1) {
+            console.log(str.search(array[i]))
+            var res = array[i];
+            break;
+        }
+    }
+    return callbackFunc(res);
+}
+
+function switchRes(res, array, posj) {
+    console.log(posj.crimeDrama)
+    console.log(res)
+    var poster = 'dead-Oscar.png'
+    switch (res) {
+        case array[0]:
+        case array[1]:
+            poster = posj.horror
+            console.log('hor')
+            break
+        case array[2]:
+        case array[3]:
+            poster = posj.history
+            console.log('his')
+            break
+        case array[4]:
+        case array[5]:
+            poster = posj.fantasy
+            console.log('fant')
+            break
+        case array[6]:
+        case array[7]:
+            poster = posj.crimeDrama
+            console.log('crime')
+            break
+        case array[8]:
+        case array[9]:
+            poster = posj.music
+            console.log('mus')
+            break
+        case array[10]:
+        case array[11]:
+            poster = posj.noirRomance
+            console.log('romance')
+            break
+        case array[12]:
+        case array[13]:
+        case array[14]:
+        case array[15]:
+        case array[16]:
+            poster = posj.action
+            console.log('act')
+            break
+        case array[17]:
+            poster = posj.sport
+            console.log('sprt')
+            break
+        case array[18]:
+        case array[19]:
+        case array[20]:
+            poster = posj.family
+            console.log('f')
+            break
+        default:
+            poster 
+    }
+    return poster
 }
